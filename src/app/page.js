03edd84d73bcc405621b69b5df6aca0ce47ed86a
4nth0ny1 <acatullo4@gmail.com>
2023-07-18 21:37:34 -0500

    created post req with api/auth/route.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import styles from "./page.module.css";
 import { useState } from "react";
 import UserProfile from "@/components/UserProfile";
 import { UserList } from "@/components/UserList";
+import { LoginForm } from "@/components/LoginForm";
 
 export default function Home() {
   const [showText, setShowText] = useState(false);
@@ -36,6 +37,7 @@ export default function Home() {
       {showText && <span>This is the text!</span>}
       <UserProfile />
       <UserList />
+      <LoginForm />
     </main>
   );
 }
