6ff0429cbca7e095810c1b9e3f14835f91bae706
4nth0ny1 <acatullo4@gmail.com>
2023-07-18 21:13:52 -0500

    added another username to handler and used server.use in the test itself

--- __tests__/UserList.test.jsx
+++ __tests__/UserList.test.jsx
@@ -1,9 +1,22 @@
 import { UserList } from "../src/components/UserList";
 import { render, screen } from "@testing-library/react";
+import { rest } from "msw";
+import { server } from "../mocks/server";
 
 describe("UserList - Rendering", () => {
   it("should should have text anthony", async () => {
     render(<UserList />);
     expect(await screen.findByText("anthony")).toBeInTheDocument();
+    expect(screen.queryByText("No Users")).not.toBeInTheDocument();
+  });
+
+  it("should have username mike rendered", async () => {
+    server.use(
+      rest.get("/api/users", (req, res, ctx) => {
+        return res(ctx.json([{ id: 2, username: "mike" }]));
+      })
+    );
+    render(<UserList />);
+    expect(await screen.findByText("mike")).toBeInTheDocument();
   });
 });
--- mocks/handlers.js
+++ mocks/handlers.js
@@ -2,6 +2,11 @@ import { rest } from "msw";
 
 export const handlers = [
   rest.get("/api/users", (req, res, ctx) => {
-    return res(ctx.json([{ id: 1, username: "anthony" }]));
+    return res(
+      ctx.json([
+        { id: 1, username: "anthony" },
+        { id: 2, username: "mike" },
+      ])
+    );
   }),
 ];
