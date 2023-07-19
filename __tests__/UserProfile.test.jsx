9b9a7d2cf503bda828283e4fabab25c7f3c3f05b
4nth0ny1 <acatullo4@gmail.com>
2023-07-18 19:34:31 -0500

    rendering with props

--- __tests__/UserProfile.test.jsx
+++ __tests__/UserProfile.test.jsx
@@ -2,7 +2,7 @@ import UserProfile from "../src/components/UserProfile";
 import { render, screen } from "@testing-library/react";
 
 describe("User Profile - Rendering", () => {
-  it("should have text email verified when isEmailVerified is true", () => {
+  it("should have text email VERIFIED when isEmailVerified is TRUE", () => {
     render(
       <UserProfile
         displayName={"Anthony the Dev"}
@@ -12,5 +12,29 @@ describe("User Profile - Rendering", () => {
       />
     );
     expect(screen.getByText(/Email Verified/i)).toBeInTheDocument();
+  });
+
+  it("should have text email NOT verified when isEmailVerified is FALSE", () => {
+    render(
+      <UserProfile
+        displayName={"Anthony the Dev"}
+        username="Adev"
+        email="adev@test.com"
+        isEmailVerified={false}
+      />
+    );
+    expect(screen.queryByText(/Email Verified/i)).not.toBeInTheDocument();
+  });
+
+  it("should have text 'Email not verified' when isEmailVerified is FALSE", () => {
+    render(
+      <UserProfile
+        displayName={"Anthony the Dev"}
+        username="Adev"
+        email="adev@test.com"
+        isEmailVerified={false}
+      />
+    );
+    expect(screen.getByText(/Email not verified/i)).toBeInTheDocument();
   });
 });
